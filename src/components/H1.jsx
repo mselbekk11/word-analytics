6fc1fbcefcbd8b95148e12ab2fb0846b9afac559
mselbekk11 <mselbekk11@gmail.com>
2024-08-07 15:09:51 -0700

    dddd

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -1,6 +1,6 @@
 
 export default function H1() {
   return (
-    <div><h1 className="first-heading">Word<span className="first-heading--thin">Analytics</span></h1></div>
+    <div><h1 className="first-heading">Wordd<span className="first-heading--thin">Analytics</span></h1></div>
   )
 }
